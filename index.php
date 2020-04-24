7f12b6464b70b2ce963d67b03f078821877d138d
Itallo Vidal <48143354+ividalll666@users.noreply.github.com>
2020-04-24 03:56:13 -0300

    só vamo em mais um projeto

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<meta charset="utf-8">
+	<title> Plantas da Vivi! </title>
+
+</head>
+
+
+<body>		
+	
+		<header class="header">
+			<label> Plantinhas da Vivi! - Bem vindo! </label>
+
+			<nav>	
+				<ul class="menu">
+					<li> <a> Início </a> </li>
+					<li> <a> Galeria </a> </li>
+					<li> <a> contato </a></li>
+				</ul>
+			</nav>
+	 	</header>
+
+
+	 	
+	 	
+	 	<section class="corpo">
+	 	
+	 		<h1> Minhas Plantinhas! </h1>	
+	 		<p class="subtitulo"> Terrários feitos artesanalmente </p>
+	 		
+	 		<article class="flex">
+
+		 		<div class="foto">
+					<img src="_imagens/_b/n1.jpg">
+		 		</div>
+	
+		 		<div class="foto">
+					<img src="_imagens/_b/n2.jpg">
+		 		</div>
+
+		 		<div class="foto">
+					<img src="_imagens/_b/n3.jpg">
+		 		</div>
+
+		 		<div class="foto">
+					<img src="_imagens/_b/n4.jpg">
+		 		</div>
+
+		 		<div class="foto">
+					<img src="_imagens/_b/n5.jpg">
+		 		</div>
+
+		 		<div class="foto">
+					<img src="_imagens/_b/n5.jpg">
+		 		</div>
+
+		 	</article>
+
+
+	 	</section>
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<main> </main>
+		
+	<footer></footer>
+	
+		
+</body>
+</html>
